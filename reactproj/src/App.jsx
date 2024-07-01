c50bc5385c7f6a402037fff0c00838bc9c4fb0e6
GerardZhou <dorey1200@gmail.com>
2024-06-30 22:47:55 -0500

    upd

--- reactproj/src/App.jsx
+++ reactproj/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
+import Hello from './components/Hello';
 function App() {
 
-  return <div className="App"> Hello World!</div>
+  return (
+    <div className="App"> 
+     <Hello/>
+    </div>
+  );
+  
 }
 
 export default App
